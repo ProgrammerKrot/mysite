1406c881e951fbb676e7cb6505e6fd5b1313fc2a
ProgrammerKrot <nikitaluksa2@gmail.com>
2023-04-21 23:22:23 +0100

    add translation and changed background

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/en")
+def home_en():
+    return render_template("index_en.html")
+
+@app.route("/bel")
+def home_bel():
+    return render_template("index_bel.html")
+
+@app.route("/ru")
+def home_ru():
+    return render_template("index_ru.html")
+
+@app.route("/por")
+def home_por():
+    return render_template("index_por.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
